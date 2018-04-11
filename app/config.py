d68becea6fc42fc516d81c7712707fd91e0ae1c6
Martin Spier <spiermar@gmail.com>
2018-04-11 14:19:00 -0700

    refactor: bind to all interfaces by default

--- app/config.py
+++ app/config.py
@@ -3,6 +3,6 @@ import os
 DEBUG = True
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 STACK_DIR = 'examples'
-HOST = '127.0.0.1'
+HOST = '0.0.0.0'
 PORT = 5000
 JSONIFY_PRETTYPRINT_REGULAR = False
