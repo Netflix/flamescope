01361da440d84f73fa70412e252475947e209caa
Martin Spier <spiermar@gmail.com>
2019-01-17 17:13:48 -0800

    feat: cpuprofile flame graph range filter

--- app/cpuprofile/flame_graph.py
+++ app/cpuprofile/flame_graph.py
@@ -18,6 +18,7 @@
 #    limitations under the License.
 
 import json
+import math
 import copy
 from os.path import join
 from app.common.fileutil import get_file
@@ -68,9 +69,9 @@ def parse_nodes(data):
     return nodes
 
 
-def generate_callgraph(root, node_id, nodes, stack):
+def generate_callgraph(root, node_id, ignore_ids, nodes, stack):
     node = nodes[node_id]  # break in case id doesn't exist
-    if node['function_name'] != '(idle)':
+    if node['id'] not in ignore_ids:
         if node['function_name'] == '':
             node['function_name'] = '(anonymous)'
         stack.append(node['function_name'])
@@ -78,7 +79,34 @@ def generate_callgraph(root, node_id, nodes, stack):
             root.add(stack, node['hit_count'])
         if node['children']:
             for child in node['children']:
-                generate_callgraph(root, child, nodes, copy.copy(stack))
+                generate_callgraph(root, child, ignore_ids, nodes, copy.copy(stack))
+    del nodes[node_id]
+
+
+def get_meta_ids(nodes):
+    program_node_id = None
+    idle_node_id = None
+    gc_node_id = None
+    for key, node in nodes.items():
+        if node['function_name'] == '(program)':
+            program_node_id = key
+        elif node['function_name'] == '(idle)':
+            idle_node_id = key
+        elif node['function_name'] == '(garbage collector)':
+            gc_node_id = key
+    return program_node_id, idle_node_id, gc_node_id
+
+
+def generate_stacks(node_id, nodes, stacks, current_stack):
+    node = nodes[node_id] # break in case id doesn't exist
+    if node['function_name'] == '':
+        node['function_name'] = '(anonymous)'
+    if node['function_name'] != '(root)':
+        current_stack.append(node['function_name'])
+        stacks[node_id] = current_stack
+    if node['children']:
+        for child in node['children']:
+            generate_stacks(child, nodes, stacks, copy.copy(current_stack))
     del nodes[node_id]
 
 
@@ -89,9 +117,36 @@ def cpuprofile_generate_flame_graph(filename, range_start, range_end, profile=No
         profile = json.load(f)
         f.close()
 
+    nodes = parse_nodes(profile)
+    ignore_ids = get_meta_ids(nodes)
+
     root = Node('root')
     root_id = profile['nodes'][0]['id']
-    nodes = parse_nodes(profile)
-    generate_callgraph(root, root_id, nodes, [])
 
+    # no range defined, just return the callgraph
+    if range_start is None and range_end is None:
+        generate_callgraph(root, root_id, nodes, [])
+        return root
+
+    samples = profile['samples']
+    time_deltas = profile['timeDeltas']
+    start_time = profile['startTime']
+    end_time = profile['endTime']
+    adjusted_start = (math.floor(start_time / 1000000) + range_start) * 1000000
+    adjusted_end = (math.floor(start_time / 1000000) + range_end) * 1000000
+    current_time = start_time + time_deltas[0]
+    stacks = {}
+    generate_stacks(root_id, nodes, stacks, [])
+    for index, sample in enumerate(samples):
+        if index == (len(samples) - 1): # last sample
+            break
+        delta = time_deltas[index + 1]
+        if delta < 0: # TODO: find a better way to deal with negative time deltas
+            delta = 0
+            continue
+        current_time += delta
+        if sample not in ignore_ids:
+            if current_time >= adjusted_start and current_time < adjusted_end:
+                stack = stacks[sample]
+                root.add(stack, delta)
     return root
