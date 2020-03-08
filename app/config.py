a457b86367f6d8432443ff1310c8fc46ccd4402a
Cameron Dart <cdart@anduril.com>
2020-03-08 15:21:34 -0700

    pull host/env from environment variables

--- app/config.py
+++ app/config.py
@@ -3,6 +3,6 @@ import os
 DEBUG = True
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 PROFILE_DIR = 'examples'
-HOST = '0.0.0.0'
-PORT = 5000
+HOST = os.getenv('FLAMESCOPE_HOST', '0.0.0.0')
+PORT = os.getenv('FLAMESCOPE_PORT', 5050)
 JSONIFY_PRETTYPRINT_REGULAR = False
